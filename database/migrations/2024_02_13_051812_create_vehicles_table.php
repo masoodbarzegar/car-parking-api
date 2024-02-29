a9ee8164c0e82b32ab194ee2afc873c4dcb80cd0
masood.barzegar <masood.barzegar@gmail.com>
2024-03-01 10:40:58 +1100

    Fix: Resolve issue with deleting vehicle
    This commit fixes the issue by adding delete_at column to Vehicles table to handle soft delete functionality

--- database/migrations/2024_02_13_051812_create_vehicles_table.php
+++ database/migrations/2024_02_13_051812_create_vehicles_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->foreignId('user_id')->constrained();
             $table->string('plate_number');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
