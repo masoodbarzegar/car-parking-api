b680429c24aa45473d46ced70dce606ea1eb94b1
masood.barzegar <masood.barzegar@gmail.com>
2024-03-01 16:18:42 +1100

    show zone list

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 use \App\Http\Controllers\Api\V1\Auth;
 use \App\Http\Controllers\Api\V1\VehicleController;
+use App\Http\Controllers\Api\V1\ZoneController;
 
 /*
 |--------------------------------------------------------------------------
@@ -32,3 +33,4 @@ Route::middleware('auth:sanctum')->group(function () {
 Route::post('auth/register', Auth\RegisterController::class);
 Route::post('auth/login', Auth\LoginController::class);
 
+Route::get('zones', [ZoneController::class, 'index']);
